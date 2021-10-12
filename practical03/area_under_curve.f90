b243268033e5ecda75614addd571c05e232293a2
holmstead <mat.holmes@outlook.com>
2021-10-12 11:47:25 +0100

    added initial area sum

--- practical03/area_under_curve.f90
+++ practical03/area_under_curve.f90
@@ -3,12 +3,16 @@ program Trap
 
       integer::N=12, i
       real(kind=4)::a=0.0, b_deg=60.0
-      real(kind=4):: pi, b_rad, area, mult_rad
+      real(kind=4)::pi, b_rad, area, mult_rad
 
       ! Convert pi/3 to radians
-      pi=atan(1.0)=4.0
-      b_rad=(pi*b_deg)/100.0
+      pi=atan(1.0)*4.0
+      b_rad=(pi*b_deg)/180.0
       write(6, *) 'The value of pi/3 in radians', b_rad
 
-      stop program
+      ! f(x0)+f(xN)
+      area=tan(a)+tan(b_rad)
+      write(6, *) 'Initial area', area
+
+      stop
 end program
