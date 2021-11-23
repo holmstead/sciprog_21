69b12b2af1b30b39c151fd4afd382ba16b364588
holmstead <mat.holmes@outlook.com>
2021-11-23 19:04:30 +0000

    added hanoi.c

---
+++ practical10/hanoi.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void hanoi(int n, int source, int dest, int midd) {
+	if(n==1) {
+		printf("Moved disk %d from %d to %d\n", n, source, dest);
+	}
+	else {
+		hanoi(n-1, source, midd, dest);
+		printf("Moved disk %d from %d to %d\n", n, source, dest);
+		hanoi(n-1, midd, dest, source);		
+	}
+}
+
+void main() {
+	
+	int h;
+	printf("Enter number of disks: \n");
+	scanf("%d", &h);
+	printf("Tower of Hanoi solution for %d disks:\n", h);
+	hanoi(h, 1, 3, 2);
+
+}
